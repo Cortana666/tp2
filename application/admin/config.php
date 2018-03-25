31d80c39145852d10cb0fb4af5c1a317552b869f
catindog <catindog@catindogdeMacBook-Pro.local>
2018-03-25 22:59:29 +0800

    new

---
+++ application/admin/config.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'template'               => [
+        'view_suffix'  => 'htm',
+        // 模板文件名分隔符
+    ],
+
+    'view_replace_str'  =>  [
+      '__PUBLIC__'=>'/public/',
+      '__ROOT__' => '/',
+      '__ADMIN__' => 'http://tp2.com/public/static/admin',
+    ]
+];
---
+++ application/admin/controller/Admin.php
@@ -0,0 +1,33 @@
+<?php
+  namespace app\admin\controller;
+
+  // use think\view;
+  // use think\controller;
+
+  class Admin
+  {
+      public function lst()
+      {
+        return view();
+
+        // $view = new \think\view([
+        //     'view_suffix'  => 'htm',
+        // ]);
+        // return $view -> fetch();
+
+        // $view = new view([
+        //     'view_suffix'  => 'htm',
+        // ]);
+        // return $view -> fetch();
+
+        // return $this -> fetch();
+      }
+      public function add()
+      {
+        return view();
+      }
+      public function edit()
+      {
+        return view();
+      }
+  }
